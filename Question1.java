d740da2948f59aeb453ab19c171b9348dd1afd54
gorata-dube <goratadube@gmail.com>
2026-08-30 10:24:16 +0200

    Add Question 1: personal profile

---
+++ Question1.java
@@ -0,0 +1,16 @@
+public class Question1 {
+    public static void main(String[] args) {
+
+        // Declare and initialise the profile variables
+        String name = "Onalenna Kereng";
+        int age = 20;
+        double gpa = 3.6;
+        boolean fullTime = true;
+
+        // Print the profile
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Full-time: " + fullTime);
+    }
+}
