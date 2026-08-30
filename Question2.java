2d34d53307676ee7b0f54bf699f476bc6b425bd6
gorata-dube <goratadube@gmail.com>
2026-08-30 10:25:52 +0200

    Add Question 2: primitive playground

---
+++ Question2.java
@@ -0,0 +1,24 @@
+public class Question2 {
+    public static void main(String[] args) {
+
+        // Declare and initialise one variable of each primitive type
+        byte byteValue = 100;
+        short shortValue = 20000;
+        int intValue = 350000;
+        long longValue = 9000000000L;
+        float floatValue = 3.14f;
+        double doubleValue = 3.14159265;
+        char charValue = 'J';
+        boolean booleanValue = true;
+
+        // Print each variable with its type
+        System.out.println("byte: " + byteValue);
+        System.out.println("short: " + shortValue);
+        System.out.println("int: " + intValue);
+        System.out.println("long: " + longValue);
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+        System.out.println("char: " + charValue);
+        System.out.println("boolean: " + booleanValue);
+    }
+}
